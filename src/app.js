2d9e9b6f1e34b542af7a22f62a5d80b9881d69d4
salah <tabitsalah12@gmail.com>
2026-02-17 11:43:52 +0000

    create all the front end pages

--- src/app.js
+++ src/app.js
@@ -1,2 +1,30 @@
-import {QuizUI} from "./modal/ui.js";
-QuizUI()
+import {QuizUI,DispalyTach,FomulaireAI} from "./modal/ui.js";
+// QuizUI()
+// let btn1=document.getElementById("btnQWIZ")
+// btn1.addEventListener("click",()=>{
+//     //mn lhna ila bgiti tjib les reponse dyal quiz
+// console.log("hello word")
+// document.getElementById("QuizP").style.display="none"
+// })
+
+
+// DispalyTach()
+
+// let ter=document.getElementById("btnTr")
+// let pass=document.getElementById("btnps")
+
+// ter.addEventListener("click",()=>{
+//     // hna l code mnin gadi tmxi l formulaire add
+//     console.log("button terminer")
+// })
+
+// pass.addEventListener("click",()=>{
+//     console.log("passer terminer")
+//     // hna l code bach delete the task 
+// })
+
+// FomulaireAI()
+// let btnAT=document.getElementById("addTask")
+// btnAT.addEventListener("click",()=>{
+//     console.log("Add Button")
+// })
